3c304a2133c7fa7193a2ee99eea55bee36c979ae
spud-69 <58670229+spud-69@users.noreply.github.com>
2020-03-07 13:48:26 -0500

    Create 54HouseFm.js

---
+++ Commands/Radio/54HouseFm.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+
+     if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+          .then(async connection => {
+            let embed = new Discord.RichEmbed();
+            embed.setDescription('**Connected!** Playing 54HouseFm Radio.');
+             embed.setColor('#A65EA5');
+
+          let dispatcher = connection.playArbitraryInput(`http://main.54house.fm/_listen/main/192.m3u`,{
+              seek: 0,
+              volume: 2,
+              passes: 1,
+              bitrate: 49000
+            })
+            message.channel.send(embed);
+          })
+          .catch(console.log);
+      } else {
+        message.reply('You are not in a voice channel!');
+      }
+    };
+  
+module.exports.help = {
+  name: "r5",
+  description: "Plays 54HosueFM Radio",
+  dev: true
+}
